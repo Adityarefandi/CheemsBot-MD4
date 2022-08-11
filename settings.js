98aaa9a77e1282fa27cb9ae0b0a14ab197ad1fb6
Aditya Refandi <108258298+Adityarefandi@users.noreply.github.com>
2022-08-11 19:48:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.APIKeys = {
 global.autoTyping = false //autoketik otomatis di gc (true buat on, false buat off)
 global.autoreadpmngc = true //pembacaan otomatis di pc (true to buat on, false buat off)
 global.autoReadGc = true //pembacaan otomatis di gc (true to on, false to off)
-global.autoRecord = true //perekaman otomatis (true to on, false to off)
+global.autoRecord = false //perekaman otomatis (true to on, false to off)
 global.available = true //tanda online (true to on, false to off)
 
 //semt menu variants
@@ -37,23 +37,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc" //ur owner name
-global.ytname = "YT: ARULL OFC" //ur yt chanel name
-global.socialm = "GitHub: Warikrr" //ur github or insta name
-global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
+global.vcardowner = ['6282244877433'] //ur owner number
+global.ownername = "𝙰𝚍𝚒𝚝𝚢𝚊 𝚁𝚎𝚏𝚊𝚗𝚍𝚒☁︎" //ur owner name
+global.ytname = "YT: Dit'z Chn Ofc" //ur yt chanel name
+global.socialm = "GitHub: Adityarefandi" //ur github or insta name
+global.location = "Boyolali, Jawa Tengah, Indonesia" //ur location
 
 //bot bomdy 
-global.owner = ['6285822347348']
-global.ownertag = '6285822347348' //ur tag number
-global.botname = 'ArullBotzMD' //ur bot name
+global.owner = ['6282244877433']
+global.ownertag = '6282244877433' //ur tag number
+global.botname = 'D͟i͟t͟´z͟_B͟o͟t☘︎' //ur bot name
 global.linkz = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/GdXv5LxKfDPHJ4WOH2rNu5" //ur website to be displayed
-global.botscript = 'https://youtu.be/k3QkeVdAMOs' //script link
-global.reactmoji = "🗿" //ur menu react emoji
+global.websitex = "https://chat.whatsapp.com/KosmbTPMlB2DvXh1R93ns0" //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/KosmbTPMlB2DvXh1R93ns0' //script link
+global.reactmoji = "❓" //ur menu react emoji
 global.themeemoji = "😩" //ur theme emoji
 global.packname = "Sticker by" //ur sticker watermark packname
-global.author = "ArullOfc" //ur sticker watermark author
+global.author = "Dit'z_Bot" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -62,7 +62,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['6285822347348'] //ur premium numbers
+global.premium = ['6282244877433'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
